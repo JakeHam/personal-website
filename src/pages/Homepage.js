7935a77fddcd6c1d3592db726c0d7c3e64b87161
Jake Hamel <jake@jakehamel.dev>
2021-02-28 17:28:35 -0800

    Customize base cra

---
+++ src/pages/Homepage.js
@@ -0,0 +1,16 @@
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+        <h1>Jake Hamel</h1>
+        <h2><i>Web Developer</i></h2>
+      </header>
+      <hr />
+      <main>
+        <h3>My Apps</h3>
+      </main>
+    </div>
+  );
+}
+
+export default Homepage;
